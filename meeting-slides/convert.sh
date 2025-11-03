3cfe6c276c1502d4a9e7a10642f9d2af71a29688
nicojmn <nicolasjeanmenne@gmail.com>
2025-11-03 13:58:24 +0100

    Improved convert script

--- meeting-slides/convert.sh
+++ meeting-slides/convert.sh
@@ -3,12 +3,37 @@
 SRC="."
 OUT="./out"
 
+while getopts "p" opt; do
+  case $opt in
+    p)
+      COMPILE_ALL=true
+      ;;
+    *)
+      echo "Usage: $0 [-p] (recompile previously build PDF)"
+      exit 1
+      ;;
+  esac
+done
+
 if [ ! -d "$OUT" ]; then
 	mkdir -p "$OUT"
 fi
 
+if [ "$COMPILE_ALL" == true ]; then
+	for md in "$SRC"/*.md; do
+		base=$(basename "$md" .md)
+		pdf="$OUT/$base.pdf"
+		marp --pdf --allow-local-files --html -o $"$pdf" "$md"
+	done
+else
 for md in "$SRC"/*.md; do
-	base=$(basename "$md" .md)
+    base=$(basename "$md" .md)
     pdf="$OUT/$base.pdf"
-	marp --pdf --allow-local-files --html -o $"$pdf" "$md"
-done
+
+    
+    if [ ! -f "$pdf" ]; then
+      marp --pdf --allow-local-files --html -o $"$pdf" "$md"
+    fi
+  done
+fi
+
